7603e6be998902ce1c4670e951215ee167d933fd
Inseok Lee <git@dlun.ch>
2022-07-12 21:44:43 +0900

    Find jump table

--- src/finder.rs
+++ src/finder.rs
@@ -1,4 +1,5 @@
 use anyhow::{anyhow, Result};
+use capstone::arch::x86;
 use object::{Object, ObjectSection};
 use tokio::fs;
 
@@ -9,7 +10,7 @@ use crate::{
 
 pub struct Finder {
     dispatch_insns: Vec<Instruction>,
-    jump_table_offset: usize,
+    jump_table_offset: u64,
 }
 
 impl Finder {
@@ -20,12 +21,12 @@ impl Finder {
 
         let code = text_section.data()?;
 
-        let dispatch = Self::search_pattern(code, &patterns::DISPATCH_PACKET_PATTERN).unwrap();
-        log::info!("dispatch: {:x}", text_section.address() + dispatch as u64);
+        let dispatch_base = Self::search_pattern(code, &patterns::DISPATCH_PACKET_PATTERN).unwrap();
+        log::info!("dispatch: {:x}", text_section.address() + dispatch_base as u64);
 
-        let dispatch_insns = disassemble_method(&code[dispatch..], text_section.address())?;
-
-        let jump_table_offset = Self::find_jump_table(&dispatch_insns);
+        let dispatch_insns = disassemble_method(&code[dispatch_base..], text_section.address() + dispatch_base as u64)?;
+        let jump_table_offset = Self::find_jump_table(object.relative_address_base(), &dispatch_insns).unwrap();
+        log::info!("jump table: {:x}", jump_table_offset);
 
         Ok(Self {
             dispatch_insns,
@@ -37,8 +38,21 @@ impl Finder {
         None
     }
 
-    fn find_jump_table(insns: &[Instruction]) -> usize {
-        0
+    fn find_jump_table(image_base: u64, insns: &[Instruction]) -> Option<u64> {
+        // find `mov ecx, DWORD PTR [r8+rax*4+<jump table address>]`
+        // assumes r8 is image base, rax is the index of the jump table
+
+        for insn in insns {
+            if insn.mnemonic == x86::X86Insn::X86_INS_MOV {
+                if let x86::X86OperandType::Mem(mem) = insn.operands[1].op_type {
+                    if mem.base().0 as u32 == x86::X86Reg::X86_REG_R8 && mem.index().0 as u32 == x86::X86Reg::X86_REG_RAX && mem.scale() == 4 {
+                        return Some(image_base + mem.disp() as u64);
+                    }
+                }
+            }
+        }
+
+        None
     }
 
     fn search_pattern(code: &[u8], pattern: &[u8]) -> Option<usize> {
