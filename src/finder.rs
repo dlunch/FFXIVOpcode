944a077006217e8a35c8bea7cf5e14228d270ef6
Inseok Lee <git@dlun.ch>
2022-07-13 17:03:11 +0900

    Find opcode

--- src/finder.rs
+++ src/finder.rs
@@ -9,8 +9,11 @@ use crate::{
 };
 
 pub struct Finder {
+    file: Vec<u8>,
     dispatch_insns: Vec<Instruction>,
     jump_table_offset: u64,
+    va_offset: u64,
+    base_address: u64,
 }
 
 impl Finder {
@@ -28,13 +31,65 @@ impl Finder {
         let jump_table_offset = Self::find_jump_table(object.relative_address_base(), &dispatch_insns).unwrap();
         log::info!("jump table: {:x}", jump_table_offset);
 
+        let va_offset = text_section.address() - (text_section.data()?.as_ptr() as usize - file.as_ptr() as usize) as u64;
+        log::info!("va offset: {:x}", va_offset);
+
+        let base_address = object.relative_address_base();
+        log::info!("base addrses: {:x}", base_address);
+
         Ok(Self {
+            file,
             dispatch_insns,
             jump_table_offset,
+            va_offset,
+            base_address,
         })
     }
 
     pub fn find_opcode(&self, handler_pattern: &[u8]) -> Option<u16> {
+        let max_opcode = 1000;
+        let opcode_base = 101;
+
+        let handler_base = self.va_offset + Self::search_pattern(&self.file, handler_pattern).unwrap() as u64;
+        log::info!("handler: {:x}", handler_base);
+
+        let handler_call_idx = self.find_dispatch_xref_insn(handler_base).unwrap();
+
+        // find jump table target
+        let mut jump_target = 0;
+        for insn in self.dispatch_insns.iter().rev().skip(self.dispatch_insns.len() - handler_call_idx) {
+            if insn.mnemonic == x86::X86Insn::X86_INS_JMP {
+                break;
+            }
+            jump_target = insn.address;
+        }
+
+        log::info!("jump target: {:x}", jump_target);
+
+        let jump_table_begin = (self.jump_table_offset - self.va_offset) as usize;
+
+        for (i, offset) in self.file[jump_table_begin..jump_table_begin + 4 * max_opcode].chunks(4).enumerate() {
+            let current_jump_target = u32::from_le_bytes(offset.try_into().unwrap()) as u64 + self.base_address;
+
+            if current_jump_target == jump_target {
+                return Some(i as u16 + opcode_base);
+            }
+        }
+
+        None
+    }
+
+    fn find_dispatch_xref_insn(&self, handler_base: u64) -> Option<usize> {
+        for (i, insn) in self.dispatch_insns.iter().enumerate() {
+            if insn.mnemonic == x86::X86Insn::X86_INS_CALL {
+                if let x86::X86OperandType::Imm(imm) = insn.operands[0].op_type {
+                    if imm as u64 == handler_base {
+                        return Some(i);
+                    }
+                }
+            }
+        }
+
         None
     }
 
